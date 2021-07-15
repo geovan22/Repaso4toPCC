76e6d2459356c3bbbd5bd664ce4e2d69ed0e7d0a
geovan22 <geovan221315@hotmail.com>
2021-07-15 08:18:47 -0600

    Instrucciones

--- Repaso/Repaso/Program.cs
+++ Repaso/Repaso/Program.cs
@@ -10,6 +10,15 @@ namespace Repaso
     {
         static void Main(string[] args)
         {
+            /*
+                Crear un sistema de inventarios que permita llevar 
+                el control de entrada y salida de productos.
+                Requisitos.
+                1.- nombre del producto
+                2.- descripcion del producto
+                3.- precio
+                4.- cantidad
+             */
         }
     }
 }
